aa648c4dabfaeb1af19d301df5e23b65b8de2abf
Mohit Garg <mohit.garg@nagarro.com>
2020-06-14 13:48:43 +0530

    Add code for list and add components

--- src/com/systemdependency/sofware/SystemDependency.java
+++ src/com/systemdependency/sofware/SystemDependency.java
@@ -1,6 +1,8 @@
 package com.systemdependency.sofware;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,30 +10,105 @@ import java.util.Map;
 public class SystemDependency {
 
 	public static void main(String[] args) {
-		String commands = "DEPEND TELNET TCPIP NETCARD\r\n" + 
-				"DEPEND TCPIP NETCARD\r\n" + 
-				"DEPEND DNS TCPIP NETCARD\r\n" + 
-				"DEPEND BROWSER TCPIP HTML\r\n" + 
-				"INSTALL NETCARD\r\n" + 
-				"INSTALL TELNET\r\n" + 
-				"INSTALL foo\r\n" + 
-				"REMOVE NETCARD\r\n" + 
-				"INSTALL BROWSER\r\n" + 
-				"INSTALL DNS\r\n" + 
-				"LIST\r\n" + 
-				"REMOVE TELNET\r\n" + 
-				"REMOVE NETCARD\r\n" + 
-				"REMOVE DNS\r\n" + 
-				"REMOVE NETCARD\r\n" + 
-				"INSTALL NETCARD\r\n" + 
-				"REMOVE TCPIP\r\n" + 
-				"REMOVE BROWSER\r\n" + 
-				"REMOVE TCPIP\r\n" + 
-				"END";
-		
-		System.out.println(args[0].split("\\s")[1]);
-		Map<String, DependendtComponents> dependentComponentsMap = new HashMap<String, DependendtComponents>();
-		List<String> installedSoftware = new ArrayList<String>();
+		if(args.length == 0) {
+			System.out.println("No command found");
+		}else {
+			String[] commands = args[0].split("\\n");
+			Map<String, DependendtComponents> dependentComponentsMap = new HashMap<String, DependendtComponents>();
+			List<String> installedSoftware = new ArrayList<String>();
+			renderSystemDependency(commands, dependentComponentsMap, installedSoftware);
+		}
 	}
+
+	private static void renderSystemDependency(String[] commands,
+			Map<String, DependendtComponents> dependentComponentsMap, List<String> installedSoftware) {
+		for (int commandCounter=0; commandCounter<commands.length; commandCounter++){
+			
+			String[] splitCommand = commands[commandCounter].split("\\s");
+			
+			if (splitCommand[0].equals("DEPEND")){
+				takeDependCommandAction(splitCommand, dependentComponentsMap);
+			}
+			if(splitCommand[0].equals("INSTALL")){
+				takeInstallMethodAction(dependentComponentsMap, installedSoftware, splitCommand);
+			}
+			if(splitCommand[0].equals("LIST")){
+				takeListMethodAction(installedSoftware);
+			}
+			if(splitCommand[0].equals("REMOVE")){
+				takeRemoveMethodAction(dependentComponentsMap, installedSoftware, splitCommand);
+			}
+		}
+		
+	}
+
+	/**
+	 * @param dependentComponentsMap
+	 * @param installedSoftware
+	 * @param splitCommand
+	 */
+	private static void takeRemoveMethodAction(Map<String, DependendtComponents> dependentComponentsMap,
+			List<String> installedSoftware, String[] splitCommand) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	/**
+	 * @param installedSoftware
+	 */
+	private static void takeListMethodAction(List<String> installedSoftware) {
+		System.out.println("List ");
+		for(String installedSofware : installedSoftware) {
+			System.out.println("  "+installedSofware);
+		}
+		
+	}
+
+	/**
+	 * @param dependentComponentsMap
+	 * @param installedSoftware
+	 * @param splitCommand
+	 */
+	private static void takeInstallMethodAction(Map<String, DependendtComponents> dependentComponentsMap,
+			List<String> installedSoftware, String[] splitCommand) {
+		String componentToInstall =  splitCommand[1];
+		System.out.println("Install "+ componentToInstall);
+		if(dependentComponentsMap.get(componentToInstall) != null) {
+			DependendtComponents dependentComponents = dependentComponentsMap.get(componentToInstall);
+			boolean isSubComponentAlreadyInstalled = true;
+			for(String subDependentComponent : dependentComponents.getDependentComponents()) {
+				if(!installedSoftware.contains(subDependentComponent)) {					
+					System.out.println("  Installing "+ subDependentComponent);
+					System.out.println("  Installing "+ componentToInstall);
+					isSubComponentAlreadyInstalled = false;
+					installedSoftware.add(subDependentComponent);
+				}
+			}
+			if(isSubComponentAlreadyInstalled) {
+				System.out.println("  Installing "+ componentToInstall);
+			}
+			installedSoftware.add(componentToInstall);
+			
+		}else if(installedSoftware.contains(componentToInstall)) {
+			System.out.println("  "+componentToInstall +" is already installed");
+		}else {
+			System.out.println("  Installing " + componentToInstall );
+			installedSoftware.add(componentToInstall);
+		}
+	}
+
+	private static void takeDependCommandAction(String[] splitCommand,
+			Map<String, DependendtComponents> dependentComponentsMap) {
+	
+		System.out.println(Arrays.toString(splitCommand));
+		List<String> dependentComponents = new ArrayList<String>();
+		for(int splitCommandCounter = 2; splitCommandCounter < splitCommand.length; splitCommandCounter++) {
+			dependentComponents.add(splitCommand[splitCommandCounter]);
+		}
+		dependentComponentsMap.put(splitCommand[1], new DependendtComponents(dependentComponents));	
+	}
+	
+	
 
 }
